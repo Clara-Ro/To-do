152c375ea3ef1e1200409b27e0301ba9603cf1fe
Clara-Ro <lara.rhee.gree@gmail.com>
2022-02-02 19:00:47 -0300

    Projeto iniciado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Task from'./components/task/'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img src="/assets/images/bg-desktop-dark.jpg" alt="background"/>
+      <div className="background">
+
+      </div>
+      <Task/>
+    </>
   );
 }
 
---
+++ src/components/task/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+export default function Task() {
+    return (
+        <div className='container'>
+            <div className='text'>
+                <h1>To Do</h1>
+                <img src="/assets/icons/icon-sun.svg" alt="sol"/>
+            </div>
+        </div>
+    )
+}
